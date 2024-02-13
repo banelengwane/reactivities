688076842901a32b2075fd1f014b671f6ebee496
banelengwane <banelengwane@gmail.com>
2024-02-13 13:23:17 +0200

    Adding an API controller

--- Persistence/Migrations/20240213093352_InitialCreate.Designer.cs
+++ Persistence/Migrations/20240213093352_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20240213090811_InitialCreate")]
+    [Migration("20240213093352_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
