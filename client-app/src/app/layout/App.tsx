2dc391bda05c6921ab6319d716f3cbcb4019eda1
banelengwane <banelengwane@gmail.com>
2024-02-15 03:14:55 +0200

    Creating an activity dashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react'
 import './styles.css'
 import axios from 'axios';
-import { Container, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -16,19 +17,13 @@ function App() {
   }, [])
 
   return (
-    <div>
+    <>
       <NavBar />
       <Container style={{marginTop: '7em'}}>
-        <List>
-          {activities.map(activity => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
+        <ActivityDashboard activities={activities} />
       </Container>
       
-    </div>
+    </>
   )
 }
 
