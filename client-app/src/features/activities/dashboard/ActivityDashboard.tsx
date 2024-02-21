6126d96819808e3ca0e6f498f8cf0eb6be5ef04d
banelengwane <banelengwane@gmail.com>
2024-02-21 19:56:33 +0200

    Cleaning up unused code

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -6,11 +6,12 @@ import { useEffect } from 'react'
 import LoadingComponent from '../../../app/layout/LoadingComponent'
 
 const ActivityDashboard = () => {    
-  const {activityStore} = useStore();  
+  const { activityStore } = useStore();  
+  const { loadActivities, activityRegistry } = activityStore;
   
   useEffect(()=> {
-    activityStore.loadActivities();
-  }, [activityStore])  
+    if(activityRegistry.size <= 1) loadActivities()
+  }, [loadActivities, activityRegistry.size])  
 
   if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />
 
