0bf120b11737361c9d5e48c6b4bfb7638263cdc9
banelengwane <banelengwane@gmail.com>
2024-02-28 04:54:24 +0200

    configuring AutoMapper profiles

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,12 +1,11 @@
 
 using Application.Activities;
 using Domain;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    
+
     public class ActivitiesController : BaseApiController
     {
         
