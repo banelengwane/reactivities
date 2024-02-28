a5110e286939ff7953a983537565e172be0d74e5
banelengwane <banelengwane@gmail.com>
2024-02-28 04:32:22 +0200

    shaping the related data

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Profiles;
 
-namespace Domain
+namespace Application.Activities
 {
-    public class Activity
+    public class ActivityDto
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
@@ -11,6 +16,7 @@ namespace Domain
         public string Category { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
-        public ICollection<ActivityAttendee> Attendees { get; set; } = new List<ActivityAttendee>();
-    }   
+        public string HostUsername { get; set; }
+        public ICollection<Profile> Profiles { get; set; }
+    }
 }
