6ec3cfec879164bdac7441371220d87430f2b9d7
banelengwane <banelengwane@gmail.com>
2024-02-29 09:28:20 +0200

    updating create and edit methods

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Activity } from "../../../app/models/activity";
+import { ActivityFormValues } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { Formik, Form } from "formik";
 import * as Yup from 'yup';
@@ -16,38 +16,33 @@ import {v4 as uuid} from 'uuid';
 
 const ActivityForm = () => {
   const {activityStore} = useStore();
-  const {createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
+  const {createActivity, updateActivity, loadActivity, loadingInitial} = activityStore;
   const {id} = useParams()
   const navigate = useNavigate();
   
-  const [activity, setActivity] = useState<Activity>({
-    id: '',
-    title: '',
-    category: '',
-    description: '',
-    date: null,
-    city: '',
-    venue: ''
-  })
+  const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues())
 
   const validationSchema = Yup.object({
     title: Yup.string().required('The activity title is required'),
     description: Yup.string().required('The activity description is required'),
     category: Yup.string().required(),
-    date: Yup.string().required("Date is required"),
+    date: Yup.string().required("Date is required").nullable(),
     city: Yup.string().required(),
     venue: Yup.string().required(),
 
   })
 
   useEffect(() => {
-    if(id) loadActivity(id).then(activity => setActivity(activity!));
+    if(id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)))
   }, [id, loadActivity])
 
-  function handleFormSubmit(activity: Activity){
+  function handleFormSubmit(activity: ActivityFormValues){
     if(!activity.id){
-      activity.id = uuid();
-      createActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+      const newActivity = {
+        ...activity,
+        id: uuid()
+      }
+      createActivity(newActivity).then(() => navigate(`/activities/${newActivity.id}`))
     } else {
       updateActivity(activity).then(() => navigate(`/activities/${activity.id}`))
     }
@@ -76,7 +71,7 @@ const ActivityForm = () => {
             <MyTextInput placeholder='Venue' name='venue' />
             <Button 
               disabled={isSubmitting || !dirty || !isValid}
-              loading={loading} 
+              loading={isSubmitting} 
               floated="right"
               positive 
               type="submit" 
