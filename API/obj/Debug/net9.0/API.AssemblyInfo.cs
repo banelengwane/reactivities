da06d719a60ed25a6addf725a7eb926b2c391340
banelengwane <banelengwane@gmail.com>
2025-07-30 12:22:12 +0200

    fetching data from the API

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc0fb360b14ead03b19c98a628c821e93cfeed3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12303495589bd136007b8c9246214b2d4381e17d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
